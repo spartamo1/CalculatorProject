26d2581fdd96225305e705d60b2566497ea1a841
momo <ptjkjm1@gmail.com>
2026-06-04 19:50:36 +0900

    refactor: 문제 조건에 따라 재귀를 반복문으로 변경

--- src/main/java/com/mosparta/calculator/CalculatorStep1.java
+++ src/main/java/com/mosparta/calculator/CalculatorStep1.java
@@ -8,42 +8,42 @@ public class CalculatorStep1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자 입력 (혹은 종료하려면 exit): ");
-        String fstInput = scanner.next();
-        if (fstInput.equals("exit"))
-            return;
-        int num1 = Integer.parseInt(fstInput);
-        System.out.println();
+        while (true) {
+            System.out.print("첫 번째 숫자 입력 (혹은 종료하려면 exit): ");
+            String fstInput = scanner.next();
+            if (fstInput.equals("exit"))
+                return;
+            int num1 = Integer.parseInt(fstInput);
+            System.out.println();
 
-        System.out.print("두 번째 숫자 입력: ");
-        int num2 = scanner.nextInt();
-        System.out.println();
+            System.out.print("두 번째 숫자 입력: ");
+            int num2 = scanner.nextInt();
+            System.out.println();
 
-        System.out.print("연산자 입력 (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
-        System.out.println();
+            System.out.print("연산자 입력 (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+            System.out.println();
 
-        switch (operator) {
-            case '+':
-                System.out.println("Result: " + (num1 + num2));
-                break;
-            case '-':
-                System.out.println("Result: " + (num1 - num2));
-                break;
-            case '*':
-                System.out.println("Result: " + (num1 * num2));
-                break;
-            case '/':
-                if (num2 != 0) {
-                    System.out.println("Result: " + (num1 / num2));
-                } else {
-                    System.out.println("Error: 0으로 나눌 수 없습니다");
-                }
-                break;
-            default:
-                System.out.println("Error: 알 수 없는 기호입니다");
+            switch (operator) {
+                case '+':
+                    System.out.println("Result: " + (num1 + num2));
+                    break;
+                case '-':
+                    System.out.println("Result: " + (num1 - num2));
+                    break;
+                case '*':
+                    System.out.println("Result: " + (num1 * num2));
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        System.out.println("Result: " + (num1 / num2));
+                    } else {
+                        System.out.println("Error: 0으로 나눌 수 없습니다");
+                    }
+                    break;
+                default:
+                    System.out.println("Error: 알 수 없는 기호입니다");
+            }
         }
-
-        main(args);
     }
 }
